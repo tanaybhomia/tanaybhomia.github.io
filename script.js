02d71a526654c2463ecab9a81d47be5393fdbab4
Tanay Bhomia <bhomia123456789@gmail.com>
2023-12-30 07:57:12 +0530

    Work under progress

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const typingAnimation = document.querySelector(".typing-text");
-    const mainContent = document.querySelector(".main");
-    const initialText = "I am Tanay"; // The text to be typed
-    const typingDelay = 50; // Delay in milliseconds between typing each character
-    const fadeInDuration = 1000; // Duration in milliseconds for fading in the main content
+document.addEventListener('DOMContentLoaded', function() {
 
-    // Function to simulate typing animation
-    const typeText = () => {
-        let charIndex = 0;
-        const typeInterval = setInterval(() => {
-            if (charIndex < initialText.length) {
-                typingAnimation.textContent += initialText.charAt(charIndex);
-                charIndex++;
-            } else {
-                // Typing animation complete
-                clearInterval(typeInterval);
+    // Function to apply slow text display
+    function displaySlowly(element, text, delay) {
+        var index = 0;
+        var intervalId = setInterval(function() {
+            element.innerHTML += text[index];
+            index++;
 
-                // Fade in the main content
-                setTimeout(() => {
-                    mainContent.style.transition = `opacity ${fadeInDuration}ms ease-in-out`;
-                    mainContent.style.opacity = 1;
-                }, fadeInDuration);
+            if (index === text.length) {
+                clearInterval(intervalId);
             }
-        }, typingDelay);
-    };
+        }, delay);
+    }
 
-    // Start the typing animation
-    typeText();
+	const name = "TANAY BHOMIA.";
+	var text = document.getElementById("slowText");
+
+	// Call the function for each target element
+    displaySlowly(slowText, name, 120);
 });
